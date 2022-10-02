37767525c6ab4c2065e196e98be95a1f40505833
Bryan Chow <bcho0024@student.monash.edu>
2022-10-03 00:40:35 +0800

    changed image data type to BYTEA

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from db import db
+from sqlalchemy.dialects.postgresql import BYTEA
 
 class Img(db.Model):
     # id = db.Column(db.Integer, primary_key=True)
-    img = db.Column(db.LargeBinary, nullable=False)
+    img = db.Column(BYTEA, nullable=False)
     name = db.Column(db.String, nullable=False, primary_key=True)
     mimetype = db.Column(db.String, nullable=False)
