e6467744d79a97bfdde0989903053cc96d4327dd
Bryan Chow <bcho0024@student.monash.edu>
2022-10-03 01:22:37 +0800

    testing without actual image

--- models.py
+++ models.py
@@ -3,6 +3,6 @@ from sqlalchemy.dialects.postgresql import BYTEA
 
 class Img(db.Model):
     # id = db.Column(db.Integer, primary_key=True)
-    img = db.Column(BYTEA, nullable=False)
+    # img = db.Column(BYTEA, nullable=False)
     name = db.Column(db.String, nullable=False, primary_key=True)
     mimetype = db.Column(db.String, nullable=False)
