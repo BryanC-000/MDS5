ab2bb28a4c0dedd385a8f468d17c98c4b95a0b9d
Bryan Chow <bcho0024@student.monash.edu>
2022-10-03 00:22:50 +0800

    changed from text to string

--- models.py
+++ models.py
@@ -3,5 +3,5 @@ from db import db
 class Img(db.Model):
     # id = db.Column(db.Integer, primary_key=True)
     img = db.Column(db.LargeBinary, nullable=False)
-    name = db.Column(db.Text, nullable=False, primary_key=True)
-    mimetype = db.Column(db.Text, nullable=False)
+    name = db.Column(db.String, nullable=False, primary_key=True)
+    mimetype = db.Column(db.String, nullable=False)
