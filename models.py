334d930c8d9c25412139763d636548fedde7bf41
Bryan Chow <bcho0024@student.monash.edu>
2022-10-03 00:03:39 +0800

    update data model

--- models.py
+++ models.py
@@ -2,6 +2,6 @@ from db import db
 
 class Img(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    img = db.Column(db.LargeBinary, nullable=False)
+    img = db.Column(db.LargeBinary)
     name = db.Column(db.Text, nullable=False)
     mimetype = db.Column(db.Text, nullable=False)
